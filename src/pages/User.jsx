f440c5cae081c8fa1548c0674e2e053bd967e9b3
Zola987 <laza.ivosevic87@gmail.com>
2022-03-14 10:14:39 +0100

    Add cleanup changes

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -25,7 +25,6 @@ function User() {
     bio,
     blog,
     twitter_username,
-    login,
     html_url,
     followers,
     following,
@@ -53,10 +52,6 @@ function User() {
               <figure>
                 <img src={avatar_url} alt="" />
               </figure>
-              <div className="card-body justify-end">
-                <h2 className="card-title mb-0">{name}</h2>
-                <p>{login}</p>
-              </div>
             </div>
           </div>
 
